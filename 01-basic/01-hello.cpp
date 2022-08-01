53499031006e1d09d16f080e82969b0fc9fce284
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-01 11:27:20 +0600

    project configured

---
+++ 01-basic/01-hello.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Md Jakaria on 1/8/22.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! basic" << std::endl;
+    return 0;
+}
---
+++ 02-intermediate/01-hello.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Md Jakaria on 1/8/22.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! intermediate" << std::endl;
+    return 0;
+}
